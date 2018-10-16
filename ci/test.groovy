a32ad62062b65eebcaa2e3a2b682be7c265335a0
Dorian Wang <spamzero@yeah.net>
2018-10-16 10:09:25 +0800

    lower build_tag

--- ci/test.groovy
+++ ci/test.groovy
@@ -1,23 +1,26 @@
 pipeline {
     agent any
+    environment {
+        BUILD_TAG = "${env.BUILD_TAG.toLowerCase()}"
+    }
     stages {
-        stage('Stop Old Build') {
+        stage("Stop Old Build") {
             steps {
-                milestone label: '', ordinal:  Integer.parseInt(env.BUILD_ID) - 1
-                milestone label: '', ordinal:  Integer.parseInt(env.BUILD_ID)
+                milestone label: "", ordinal:  Integer.parseInt(env.BUILD_ID) - 1
+                milestone label: "", ordinal:  Integer.parseInt(env.BUILD_ID)
             }
         }
-        stage('Test') {
+        stage("Test") {
             steps {
-                sh "make ci_up"
-                sh 'make ci_test'
-                junit 'nosetests.xml'
-                cobertura coberturaReportFile: '**/build/coverage/coverage.xml', failUnstable: false, maxNumberOfBuilds: 20, onlyStable: false, zoomCoverageChart: false
+                sh "BUILD_TAG=${BUILD_TAG} make ci_up"
+                sh "BUILD_TAG=${BUILD_TAG} make ci_test"
+                junit "nosetests.xml"
+                cobertura coberturaReportFile: "**/build/coverage/coverage.xml", failUnstable: false, maxNumberOfBuilds: 20, onlyStable: false, zoomCoverageChart: false
             }
             post {
                 always {
-                    sh 'make ci_clean'
-                    sh 'make ci_down'
+                    sh "BUILD_TAG=${BUILD_TAG} make ci_clean"
+                    sh "BUILD_TAG=${BUILD_TAG} make ci_down"
                 }
             }
         }
